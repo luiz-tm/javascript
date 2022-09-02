f1c804d4ef9f2b32cf9fd50d3d8c427c9b9bf745
Luiz Henrique <58668840+luiz-tm@users.noreply.github.com>
2022-09-01 23:05:26 -0300

    aula 80-1

--- javascript-typescript/aula80/app.js
+++ javascript-typescript/aula80/app.js
@@ -2,11 +2,23 @@ const pessoas = [
     {id: 3, nome: 'Luiz'},
     {id: 2, nome: 'Maria'},
     {id: 1, nome: 'Helena'}
-]
+];
 
-const novasPessoas = {};
-for( const {id, nome} of pessoas) {
-    novasPessoas[id] = { id, nome }
+// const novasPessoas = {}
+// for( const pessoa of pessoas)
+// {
+//     const { id } = pessoa;
+
+//     novasPessoas[id] = { ...pessoa }
+// }
+
+const novasPessoas = new Map();
+for( const pessoa of pessoas)
+{
+    const { id } = pessoa;
+    novasPessoas.set(id, { ...pessoa })
 }
 
-console.log(novasPessoas)
+for ( const [identifier, {id, nome}] of novasPessoas) {
+    console.log(identifier, id, nome)
+}
