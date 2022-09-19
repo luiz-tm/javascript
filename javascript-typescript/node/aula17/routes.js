f7f6758a41a569bdf0af80214a102ec4c957ff02
Luiz Henrique <58668840+luiz-tm@users.noreply.github.com>
2022-09-19 12:55:53 -0300

    aula 17

--- javascript-typescript/node/aula17/routes.js
+++ javascript-typescript/node/aula17/routes.js
@@ -1,13 +1,14 @@
-const express = require('express')
+const express = require('express');
 const route = express.Router();
-const homeController = require('./src/controllers/homeControllers')
-const contatoController = require('./src/controllers/contatoController')
-
+const homeController = require('./src/controllers/homeController');
+const contatoController = require('./src/controllers/contatoController');
 
 // Rotas da home
-route.get('/', homeController.paginaInicial)
+route.get('/', homeController.paginaInicial);
 route.post('/', homeController.trataPost);
 
-route.get('/contato', contatoController.paginaInicial)
+// Rotas de contato
+route.get('/contato', contatoController.paginaInicial);
+
 
 module.exports = route;
--- javascript-typescript/node/aula17/server.js
+++ javascript-typescript/node/aula17/server.js
@@ -1,30 +1,25 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
 mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.emit('pronto');
   })
   .catch(e => console.log(e));
-
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
-
 const routes = require('./routes');
 const path = require('path');
-
-const helmet = require('helmet')
-const csrf = require('csurf')
-
+const helmet = require('helmet');
+const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
-app.use(helmet())
+app.use(helmet());
+
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
@@ -43,11 +38,11 @@ app.use(flash());
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+app.use(csrf());
 // Nossos próprios middlewares
-app.use(csrf())
 app.use(middlewareGlobal);
-app.use(csrfMiddleware)
-app.use(checkCsrfError)
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
 app.on('pronto', () => {
