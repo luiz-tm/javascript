17338724e4e2cb47e4138d3492c6505c5e977c61
Luiz Henrique <58668840+luiz-tm@users.noreply.github.com>
2022-09-20 14:41:57 -0300

    criado registro e login

--- javascript-typescript/projeto-agenda/src/controllers/loginController.js
+++ javascript-typescript/projeto-agenda/src/controllers/loginController.js
@@ -1,12 +1,67 @@
 const Login = require('../models/LoginModel')
 
 exports.index = (req, res) => {
+    if(req.session.user) return res.render('login-logado')
     res.render('login')
 }
 
-exports.register = (req, res) => {
-    const login = new Login(req.body);
-    login.register()
+exports.register = async (req, res) => {
+    try {
+        const login = new Login(req.body);
+        await login.register()
 
-    res.send(login.errors)
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors)
+            req.session.save(function () {
+                return res.redirect('back')
+            })
+            return;
+        }
+
+        req.flash('success', 'Seu usuário foi criado com sucesso')
+        req.session.save(function () {
+            return res.redirect('back')
+        })
+    } catch (e) {
+        console.log(e)
+        res.render('404')
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const login = new Login(req.body);
+        await login.login()
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors)
+            req.session.save(function () {
+                return res.redirect('back')
+            })
+            return;
+        }
+
+        if (!login.user) {
+            req.flash('errors', login.errors)
+            req.session.save(function () {
+                return res.redirect('back')
+            })
+            return;
+        }
+
+        req.flash('success', 'Você efetuou login no sistema')
+        req.session.user = login.user
+        req.session.save(function () {
+            return res.redirect('back')
+        })
+    } catch (e) {
+        console.log(e)
+        res.render('404')
+    }
+}
+
+exports.logout = function(req, res) {
+    req.session.destroy();
+
+    res.redirect('/')
 }
--- javascript-typescript/projeto-agenda/src/models/LoginModel.js
+++ javascript-typescript/projeto-agenda/src/models/LoginModel.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
+const bcryptjs = require('bcryptjs')
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
-  senha: { type: String, required: true }
+  password: { type: String, required: true }
 });
 
 const LoginModel = mongoose.model('Login', LoginSchema);
@@ -37,12 +38,45 @@ class Login {
     // A senha precisa ter entre 3 e 50 caracteres
     if(this.body.password.length < 3) this.errors.push('A senha precisa ter entre 3 e 50 caracteres.')
 
-
   }
 
-  register() {
+  async userExists() {
+    this.user = await LoginModel.findOne({ email: this.body.email })
+    if(this.user) this.errors.push('Usuário já cadastrado no sistema')
+  }
+
+  async login() {
     this.valida()
     if(this.errors.length > 0) return;
+
+    if(this.errors.length > 0) return;
+
+    this.user = await LoginModel.findOne({ email: this.body.email })
+
+    if(!this.user) {
+      this.errors.push('Usuário ou senha inválidos')
+      return;
+    } 
+      
+    if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
+      this.errors.push('Senha inválida')
+      this.user = null;
+      return;
+    }
+  }
+
+  async register() {
+    this.valida()
+    if(this.errors.length > 0) return;
+
+    await this.userExists()
+
+    if(this.errors.length > 0) return;
+
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt)
+
+    this.user = await LoginModel.create(this.body)
   }
 }
 
